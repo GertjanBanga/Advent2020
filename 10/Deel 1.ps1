617fb83c479288ce40e30825e5bbeb6e8b6675d0
GertjanBanga <71268245+GertjanBanga@users.noreply.github.com>
2020-12-10 15:00:53 +0100

    Add files via upload

---
+++ 10/Deel 1.ps1
@@ -0,0 +1,27 @@
+﻿cls
+cd "C:\Users\gertjanb\OneDrive - Delta-N\Documents\Advent\10"
+$Path = ".\input.txt"
+$input= Get-Content -Path $Path
+
+$array=@()
+$eenverschil=0
+$drieverschil=0
+$vorigekabel=1
+
+Foreach ($regel in $input)
+{
+    $array+=[decimal]$regel
+}
+
+$output=($array | sort)
+
+Foreach ($kabel in $output)
+{
+    Write-host $kabel $vorigekabel
+    If (($kabel - $vorigekabel) -eq 1) {$eenverschil+=1} else {if (($kabel - $vorigekabel) -eq 3) {$drieverschil+=1}}
+    $vorigekabel=$kabel
+}
+
+$eenverschil+=1
+$drieverschil+=1
+$eenverschil*$drieverschil
---
+++ 9/Deel 1.ps1
@@ -0,0 +1,67 @@
+﻿cls
+$stopwatch =  [system.diagnostics.stopwatch]::StartNew()
+cd "C:\Users\gertjanb\OneDrive - Delta-N\Documents\Advent\9"
+$Path = ".\input.txt"
+$array= Get-Content -Path $Path
+
+$counter=0
+$counterhoofd=0
+$countercheck=25
+$arrayA=@()
+$arrayB=@()
+
+
+While ($counter -le $array.count) # Hoofd loop van begin tot eind
+{
+    $checkarray=@($array[$counter])
+    $checknumbr=$array[$countercheck]
+    While ($counter -le $countereind)
+    {
+        $arrayA=$arrayA + $array[$counter]
+        $counter+=1
+    }
+    $arrayB=$arrayA
+    Write-host "Array A" -ForegroundColor Yellow
+    $arrayA
+    Write-host "Array B" -ForegroundColor Yellow
+    $arrayB
+    #Foreach ($A in $arrayA)
+    #{
+        #Write-Host "Foreach A"
+    #    Foreach ($B in $arrayB)
+     #   {
+      #      #Write-Host "Foreach B"
+       #     $som=$A+$B
+        #    If ($som -eq $array[$countercheck]) {Write-host "Gevonden"}
+        #}
+    
+    #$arrayA=@()
+    #$arrayB=@()
+    #$counter+=1
+    #}
+    $counterhoofd+=1
+    pause
+}
+
+Pause
+
+    While ($array[$counterA] -ne $array[$countercheck])
+    {
+        $som=[decimal]$array[$counterA]+[decimal]$array[$counterB]
+        if ($counterA -ne $counterB)
+        {       
+            if ($som -eq $array[$countercheck]) {Write-Host "Gevonden 1 -- Getal" $counterA "=" $array[$counterA] "Getal" $counterB "=" $array[$counterB] "Getal A + Getal B =" $som "Getal moet zijn:" $array[$countercheck] -forgroundcolor -Yellow}
+        }
+        Write-Host "Niet gevonden -- Getal" $counterA "=" $array[$counterA] "Getal" $counterB "=" $array[$counterB] "Getal A + Getal B =" $som "Getal moet zijn:" $array[$countercheck]
+        $counterA+=1
+        While ($array[$counterA] -ne $array[$countercheck2])
+        {
+            if ($som -eq $array[$countercheck2]) {Write-Host "Gevonden 2 -- Getal" $counterA "=" $array[$counterA] "Getal" $counterB "=" $array[$counterB] "Getal A + Getal B =" $som "Getal moet zijn:" $array[$countercheck] -forgroundcolor -Yellow}
+            Write-Host "Gevonden 2 -- Getal" $counterA "=" $array[$counterA] "Getal" $counterB "=" $array[$counterB] "Getal A + Getal B =" $som "Getal moet zijn:" $array[$countercheck] -ForegroundColor Yellow
+            $counterB+=1
+        }
+    }
+    $countercheck+=1
+    pause
+}
+
